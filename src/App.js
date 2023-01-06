9b9817931c0cecac6be41ff7c0bb993ff91f54cc
nani-samireddy <nanisamireddy05@gmail.com>
2023-01-06 21:14:16 +0530

    initilized home and nav components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import Homepage from './Pages/Home/Home';
 
 function App() {
   return (
     <div className="App">
-
+      <Homepage />
     </div>
   );
 }
---
+++ src/Pages/Home/Home.js
@@ -0,0 +1,12 @@
+import Navbar from "../../components/Navbar/Navbar";
+
+const Homepage = () => {
+    return (
+        <div>
+            <Navbar />
+            <h1>Homepage</h1>
+        </div>
+    );
+}
+
+export default Homepage;
