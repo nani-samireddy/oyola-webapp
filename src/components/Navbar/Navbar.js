8799a87a681302a8dffae3bb804023e9ef99cce5
sp-chandra-sekhar <developer.spcsg@email.com>
2023-01-06 23:51:13 +0530

    nav become responsive

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,17 +1,39 @@
-import './Navbar.css';
-const Navbar = () => {
+import { React, useRef } from "react";
+import "./Navbar.css";
+import { FaBars, FaTimes } from "react-icons/fa";
 
-    return (
-        <nav>
-            <a className='title'><h1>OYOLA</h1></a>
-            <div className="links">
-                <a href="#">Packages</a>
-                <a href="#">Gallery</a>
-                <a href="#">Login</a>
-                <a className='registerButton' href="#">Register</a>
-            </div>
-        </nav>
-    );
-}
+const Navbar = () => {
+  const navRef = useRef();
+
+  const showNavBar = () => {
+    navRef.current.classList.toggle("responsive_nav");
+  };
+
+  return (
+    <div className="navBarContainer">
+      <nav>
+        <a className="title">
+          <h1>OYOLA</h1>
+        </a>
+        <div className="links" ref={navRef}>
+          <a href="#">Packages</a>
+          <a href="#">Gallery</a>
+          <a href="#">Login</a>
+          <a className="registerButton" href="#">
+            Register
+          </a>
+
+          <button className="nav-btn nav-close-btn" onClick={showNavBar}>
+            <FaTimes />
+          </button>
+        </div>
+      </nav>
+
+      <button className="nav-btn" onClick={showNavBar}>
+        <FaBars />
+      </button>
+    </div>
+  );
+};
 
 export default Navbar;
